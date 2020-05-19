f8bcf841ffedc7320223275c65bbb3343abc334c
layfredrc <frederic.lay@etu.parisdescartes.fr>
2020-05-19 17:48:57 +0200

    Read Me v1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import Player from './components/video-stream/video-streaming.component.jsx'
+import React, { Component } from "react";
+import Player from "./components/video-stream/video-streaming.component.jsx";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-
-
-  render(){
-    return (
-      <div className="App">
-         
-          <h1>Hello</h1>
-          <Player />
-      </div>
-    );
-}
- 
+	render() {
+		return (
+			<div className="App">
+				<h1>Hello</h1>
+				<Player />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/video-stream/video-streaming.component.jsx
+++ src/components/video-stream/video-streaming.component.jsx
@@ -1,52 +1,43 @@
-import React from 'react';
-import {Component} from 'react';
+import React from "react";
+import { Component } from "react";
 
-import Hls from 'hls.js';
+import Hls from "hls.js";
 
+class Player extends Component {
+	componentDidMount() {
+		const streamURL = "http://localhost:3000/assets/sample1/sample.m3u8";
+		const video = this.player;
+		const hls = new Hls();
 
+		if (Hls.isSupported() && this.player) {
+			hls.loadSource(streamURL);
+			hls.attachMedia(video);
+			hls.on(Hls.Events.MANIFEST_PARSED, function () {
+				video.play();
+			});
+		}
+	}
 
+	render() {
+		const style = {
+			width: 800,
+			height: 500,
+			background: "#000",
+		};
 
-
- class Player extends Component {
-  
-    componentDidMount(){
-
-        const streamURL = 'http://localhost:3000/assets/sample1/sample.m3u8';
-        const video = this.player;
-        const hls = new Hls();
-
-        if (Hls.isSupported() && this.player) {
-            
-            hls.loadSource(streamURL);
-            hls.attachMedia(video);
-            hls.on(Hls.Events.MANIFEST_PARSED, function() {
-              video.play();
-            });
-        }
-     
-    }
-
-
-
-    render(){
-        const style = {
-            width: 800,
-            height: 500,
-            background: '#000',
-        }
-
-
-        return (
-            <div>
-                 <video controls={true}  style={style} ref={(player) => this.player = player} autoPlay={true}> </video>
-                
-            </div>
-           
-        );
-    }
+		return (
+			<div>
+				<video
+					controls={true}
+					style={style}
+					ref={(player) => (this.player = player)}
+					autoPlay={true}
+				>
+					{" "}
+				</video>
+			</div>
+		);
+	}
 }
 
-
-  
 export default Player;
-  
